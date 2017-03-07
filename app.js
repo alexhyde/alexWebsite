3de721b99120309cb2da35f59cd6954c7185b168
Alex Hyde <ahyde@stepnetworks.com>
2017-03-07 15:49:23 -0500

    Added bootstrap template

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
-var express = require('Express');
+var express = require('express');
 var app = express();
 
+var port = 5001;
+
+app.use(express.static('public'));
+app.use(express.static('src/views'));
+app.use(express.static('bower_components'));
+
+app.get('/',function(req,res){
+	res.send('Landing page for Alex Website');
+});
+
+app.listen(port, function(err){
+	console.log('Started Alex Website on port: ' + port);
+});
